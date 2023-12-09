440c43be8914e83b4cc5d617dee358a9ccf40154
Didier <tagnedidier@gmail.com>
2023-12-09 16:38:31 +0100

    Update

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+import argparse
+from pika import BasicProperties
+
 from rabbitmq import receive_message
 
 
+def callback(ch, method, properties: BasicProperties, body: bytes):
+	print("HEADERS", properties.headers, type(properties.headers))
+
+
 if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-u", "--username", type=str, default="guest")
+	parser.add_argument("-p", "--password", type=str, default="guest")
+	parser.add_argument("-rp", "--port", type=int, default=5672)  # rabbitmq port
+	parser.add_argument("-f", "--not_durable", action="store_true")
+	parser.add_argument("-q", "--is_quorum", action="store_true")
+
+	parser.add_argument("-rh", "--host", type=str, required=True)  # rabbitmq host
+	parser.add_argument("-n", "--queue", type=str, required=True)  # queue name
+
+	args: argparse.Namespace = parser.parse_args()
+
 	receive_message(
-		queue_name="sa.message.queues.direct.notifications"
+		host=args.host,
+		port=args.port,
+		username=args.username,
+		password=args.password,
+		queue_name=args.queue,
+		message_callback=callback,
+		durable=False if args.not_durable else True,
+		arguments={"x-queue-type": "quorum"} if args.is_quorum else None
 	)
--- rabbitmq.py
+++ rabbitmq.py
@@ -4,17 +4,19 @@ from rich import print
 
 
 def receive_message(
+    host: str,
+    port: int,
+    username: str,
+    password: str,
     queue_name: str,
-    passive: bool = False,
+    message_callback,
     durable: bool = True,
-    exclusive: bool = False,
-    auto_delete: bool = False,
-    headers: dict = None
+    arguments: dict = None
 ) -> None:
     connection_params = pika.ConnectionParameters(
-        host='rabbitmq',
-        port=5672,
-        credentials=pika.PlainCredentials(username='guest', password='guest')
+        host=host,
+        port=port,
+        credentials=pika.PlainCredentials(username=username, password=password)
     )
     connection = pika.BlockingConnection(connection_params)
 
@@ -23,17 +25,15 @@ def receive_message(
     # Declare a queue
     channel.queue_declare(
         queue=queue_name,
-        passive=passive,
         durable=durable,
-        exclusive=exclusive,
-        auto_delete=auto_delete,
-        arguments=headers
+        arguments=arguments
     )
 
     def callback(ch, method, properties, body: bytes):
         print(f"[x] Received '{body.decode('utf-8')}'")
+        message_callback(ch, method, properties, body)
 
     print(f"[:] Start listen '{queue_name}'")
 
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=False)
     channel.start_consuming()
